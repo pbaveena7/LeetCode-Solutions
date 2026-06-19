cbe0284a96ff7904389613ac0fcf76d44a25f8f8
Pon Baveena E <pbaveena@gmail.com>
2026-06-19 23:23:13 +0530

    Time: 4 ms (30.17%), Space: 9.6 MB (56.92%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-       
         if (num1 == "0" || num2 == "0") return "0";
-        
         vector<int> num(num1.size() + num2.size(), 0);
-        
         for (int i = num1.size() - 1; i >= 0; --i) {
             for (int j = num2.size() - 1; j >= 0; --j) {
                 num[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
@@ -13,13 +10,10 @@ public:
                 num[i + j + 1] %= 10;
             }
         }
-        
         int i = 0;
         while (i < num.size() && num[i] == 0) ++i;
-        
         string res = "";
         while (i < num.size()) res.push_back(num[i++] + '0');
-        
         return res;
     }
 };
