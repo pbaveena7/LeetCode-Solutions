fb13831db8955adc4620df8a660af4aa4a25500e
Pon Baveena E <pbaveena@gmail.com>
2026-07-29 19:20:04 +0530

    Time: 0 ms (100%), Space: 9.3 MB (82.66%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,11 +4,10 @@ public:
         vector<int>m1(256,-1);
         vector<int>m2(256,-1);
         for(int i=0;i<s.size();i++){
-              if(m1[s[i]]!=m2[t[i]])
-              return false;
-              m1[s[i]]=i;
-              m2[t[i]]=i;
+            if(m1[s[i]]!=m2[t[i]]) return false;
+            m1[s[i]]=i;
+            m2[t[i]]=i;
         }
-       return true;
+        return true;
     }
 };
