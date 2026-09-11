3b5a42b6df3773b649aae138034ce9ea6ab190c3
Pon Baveena E <pbaveena@gmail.com>
2026-09-11 22:02:12 +0530

    Time: 3 ms (64.55%), Space: 8.2 MB (17.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+int f(int n,vector<int>&dp){
+    if(n<=1) return n;
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]=f(n-1,dp)+f(n-2,dp);
+}
     int fib(int n) {
-        if(n<=1)
-        return n;
-        return fib(n-1)+fib(n-2);
-    
-        
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
     }
 };
