0ab2cf2a068e90097e61372f62e758ffa92fd8fb
Pon Baveena E <pbaveena@gmail.com>
2026-09-11 23:10:12 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.48%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
+int f(int n,vector<int> & dp){
+    if(n<=1) return n;
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
     int fib(int n) {
-        if(n<=1) return n;
-        int prev1=0;
-        int prev2=1;
-        for(int i=2;i<=n;i++){
-            int cur=prev1+prev2;
-            prev1=prev2;
-            prev2=cur;
-        }
-        return prev2;
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
     }
 };
