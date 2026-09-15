940328e37dd40d43715703a7a753c7847b2cadf2
Pon Baveena E <pbaveena@gmail.com>
2026-09-15 19:21:12 +0530

    Time: 2 ms (5.05%), Space: 8.6 MB (38.34%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-int f(int n ){
- if(n<=1) return 1;
-   int prev1=1;
-   int prev2=1;
-   for(int i=2;i<=n;i++){
-    int curr=prev1+prev2;
-    prev1=prev2;
-    prev2=curr;
-   }
-   return prev2;
-}
+    int f(int n,vector<int>&dp){
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
+    }
     int climbStairs(int n) {
-        return f(n);
-       
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
     }
 };
