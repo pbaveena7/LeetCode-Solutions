2e6f3591d79196706d41fd608b1b841479a7fd97
Pon Baveena E <pbaveena@gmail.com>
2026-09-17 14:44:11 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.62%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+      int n=nums.size();
       int Xor=0;
-      for(int i=0;i<nums.size();i++){
+      for(int i=0;i<n;i++){
         Xor=Xor^nums[i];
       }
       return Xor;
