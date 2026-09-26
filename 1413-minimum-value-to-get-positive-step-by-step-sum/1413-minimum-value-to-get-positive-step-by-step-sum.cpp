cc46132cf6391f501510fc2c9f033a58e73ab7cd
Pon Baveena E <pbaveena@gmail.com>
2026-09-26 21:24:37 +0530

    Time: 0 ms (100%), Space: 9.8 MB (77.98%) - LeetHub

---
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        int minsum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+             minsum=min(minsum,sum);
+        }
+        return 1-minsum;
+    }
+};
